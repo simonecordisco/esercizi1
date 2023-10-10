2ab53697dd81a2b65a639cf5d898e4eff1e3d39e
simonecordisco <147503322+simonecordisco@users.noreply.github.com>
2023-10-10 14:25:11 +0200

    Add files via upload

---
+++ casting1.java
@@ -0,0 +1,9 @@
+public class MyClass {
+    public static void main(String args[]) {
+     double x = 6;
+     double y = 3;
+     int z =(int)x+(int)y;
+
+      System.out.println("x + y = " + z);
+    }
+}
