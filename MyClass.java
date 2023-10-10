55cd5a55f428f3cb889931b83518c5c80cf60e1e
simonecordisco <147503322+simonecordisco@users.noreply.github.com>
2023-10-10 14:14:14 +0200

    Add files via upload

---
+++ MyClass.java
@@ -0,0 +1,9 @@
+public class MyClass {
+    public static void main(String args[]) {
+      int x=10;
+      int y=25;
+      int z=(x*y)/2;
+
+      System.out.println(" area is = " + z);
+    }
+}
