e1aa0e2e9de6790f9b10ab19b079a54c9600be95
simonecordisco <147503322+simonecordisco@users.noreply.github.com>
2023-10-12 15:20:09 +0200

    Add files via upload

---
+++ aritmentici3.java
@@ -0,0 +1,14 @@
+
+//Scrivi un programma che contiene un metodo che date due variabili le incrementi di un valore scelto da te e le moltiplichi fra di loro.
+public class MyClass {
+    public static void main(String args[]) {
+       int x =4;
+       int y = 2;
+       System.out.println("x =" + x);
+       System.out.println("y =" + y);
+        x=x+2;
+        y=y+4;
+    int resultSum = (x*y);
+      System.out.println("risultato " + resultSum);
+    }
+}
