07a0e2234bec26e7008c3b5e8f40adf4870ad17a
simonecordisco <147503322+simonecordisco@users.noreply.github.com>
2023-10-12 14:31:57 +0200

    Add files via upload

---
+++ funzioni1.java
@@ -0,0 +1,14 @@
+public class MyClass {
+    public static void main(String args[]) {
+       double a =65;
+       double b= 4;
+
+      System.out.println("somma di a e b =" + somma (a,b));
+    }
+
+    public static int somma(double a, double b){
+    int c =(int) a+(int)b;
+    return c;
+}
+    
+}
