3b825fb001f297dd917829dc754ef3a0d06b66bd
simonecordisco <147503322+simonecordisco@users.noreply.github.com>
2023-10-12 15:08:37 +0200

    Add files via upload

---
+++ aritmetici1.java
@@ -0,0 +1,10 @@
+public class MyClass {
+    public static void main(String args[]) {
+       int x =40;
+       int y = 20;
+       System.out.println("x =" + x);
+       System.out.println("y =" + y);
+    int resultSum = x / y;
+      System.out.println("il quoziente è " + resultSum);
+    }
+}
