09da39d02a4ae0bf203176f0262221d245a5402c
simonecordisco <147503322+simonecordisco@users.noreply.github.com>
2023-10-12 15:10:39 +0200

    Add files via upload

---
+++ aritmentici2.java
@@ -0,0 +1,12 @@
+public class MyClass {
+    public static void main(String args[]) {
+       int x =40;
+       int y = 20;
+       int z = 30;
+       System.out.println("x =" + x);
+       System.out.println("y =" + y);
+        System.out.println("z =" + z);
+    int resultSum = (x+y+z)/3;
+      System.out.println("la media è " + resultSum);
+    }
+}
