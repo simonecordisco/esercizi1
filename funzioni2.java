9bfd8390488aa0adf4145ebc2d1c6b4a183dd2c4
simonecordisco <147503322+simonecordisco@users.noreply.github.com>
2023-10-12 14:57:53 +0200

    Add files via upload

---
+++ funzioni2.java
@@ -0,0 +1,8 @@
+public class MyClass {
+    public static void main(String args[]) {
+       String nome = "simone";
+       int lunghezza = nome.length();
+    
+      System.out.println(lunghezza);
+    }
+}
