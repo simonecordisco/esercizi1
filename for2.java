459dbd20283eb9041ebf2e2df25c1dd574c38d24
simonecordisco <147503322+simonecordisco@users.noreply.github.com>
2023-10-13 14:46:47 +0200

    Add files via upload

---
+++ for2.java
@@ -0,0 +1,10 @@
+//Scrivi un programma che contenga un metodo che prende in ingresso un numero e il massimo numero
+//di risultati che vuoi avere e stampi tutti i numeri naturali in ordine inverso (Quindi se passo il punto di partenza a 6 e gli dico di restituirmi 3 risultati mi aspetto in uscita [6 5 4] stampati).
+public class MyClass {
+    public static void main(String args[]) {
+      
+     for(int y=6; y>=4; y--){
+        System.out.println (y);
+      }
+      }
+}
