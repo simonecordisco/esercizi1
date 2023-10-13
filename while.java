a8ea65687ab3a8b9b3eba3d15e64cf6534b97314
simonecordisco <147503322+simonecordisco@users.noreply.github.com>
2023-10-13 14:32:48 +0200

    Add files via upload

---
+++ while.java
@@ -0,0 +1,14 @@
+
+public class MyClass {
+    public static void main(String args[]) {
+      int x=0;
+      int somma=0;
+      while (x<5){
+          x++;
+          somma = somma + x;
+
+        System.out.println ("La somma e' " + somma);
+      }
+      }
+}
+// non capisco come fermalo a 4 cioè a 10 come risultato
