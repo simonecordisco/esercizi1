0e8ef554659cd286be8217b2ab1b89c098859eb7
simonecordisco <147503322+simonecordisco@users.noreply.github.com>
2023-10-13 11:45:43 +0200

    Add files via upload

---
+++ operatorilogici2.java
@@ -0,0 +1,12 @@
+//Scrivi un programma che contiene un metodo che prenda un numero in ingresso e capisca se un numero è pari o dispari utilizzando solo gli operatori logici.
+public class MyClass {
+    public static void main(String args[]) {
+      int x = 15;
+      System.out.println(x);
+
+      
+      boolean z = (x % 2)==0;
+
+      System.out.println(z);
+    }
+}
