68ca758725881fbb9ef8266aeccb658fb673ae68
simonecordisco <147503322+simonecordisco@users.noreply.github.com>
2023-10-13 11:06:50 +0200

    Add files via upload

---
+++ operatori di comparazione2.java
@@ -0,0 +1,14 @@
+
+//Scrivi un programma che contiene un metodo che che confronti due caratteri e determini se sono diversi.
+//Il programma dovrà stampare a video i due caratteri e il risultato dell'eguaglianza.
+public class MyClass {
+    public static void main(String args[]) {
+      char x = 'x' ;
+      char y = 'y';
+      System.out.println(x);
+      System.out.println(y);
+      boolean equals = x==y;
+
+      System.out.println(equals);
+    }
+}
