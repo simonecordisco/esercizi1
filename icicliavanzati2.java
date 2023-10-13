3355b9cf4c43d7e220d8b62c7051f2f2d5468a67
simonecordisco <147503322+simonecordisco@users.noreply.github.com>
2023-10-13 14:59:52 +0200

    Add files via upload

---
+++ icicliavanzati2.java
@@ -0,0 +1,14 @@
+//Scrivi un programma che contenga un metodo che prende in ingresso un valore numerico e stampa tutti i numeri fino al valore immesso, la stampa dovrà interrompersi se il valore è uguale a 5.
+
+public class MyClass {
+    public static void main(String args[]) {
+      for(int x= 0; x<=7; x++){
+        if(x==5){
+          continue;
+         
+       }
+         System.out.println(x);
+      }
+     
+    }
+}
