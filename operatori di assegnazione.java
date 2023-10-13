dfa17d7bfb90cc3833bb20737d3d10f7605e257a
simonecordisco <147503322+simonecordisco@users.noreply.github.com>
2023-10-13 10:44:37 +0200

    Add files via upload

---
+++ operatori di assegnazione.java
@@ -0,0 +1,16 @@
+
+//Scrivi un programma che contiene un metodo che date due variabili le incrementi di un valore scelto da te e le moltiplichi fra di loro.
+//Il metodo dovrà restituire il risultato dell'operazione che dovrà essere stampato a video.
+public class MyClass {
+    public static void main(String args[]) {
+      int x=2;
+      int y=2;
+      System.out.println(x);
+      System.out.println(y);
+      x = x + 4;
+      y += 2;
+      int z=x*y;
+
+      System.out.println(z);
+    }
+}
