a637543ff5c501f18d86070048ae23960ed10376
simonecordisco <147503322+simonecordisco@users.noreply.github.com>
2023-10-13 11:21:41 +0200

    Add files via upload

---
+++ operatorilogici1.java
@@ -0,0 +1,15 @@
+
+//Scrivi un programma che contiene un metodo che dati 2 interi in ingresso ed un valore di confronto
+//verifichi se il numero di confronto è compreso tra due valori specifici e stampi il risultato dell'operazione comprensivo dei due limiti (inferiore e superiore)
+public class MyClass {
+    public static void main(String args[]) {
+      int x = 10 ;
+      int y = 20;
+      System.out.println(x);
+      System.out.println(y);
+      
+      boolean z = x<10 && y>30;
+
+      System.out.println(z);
+    }
+}
