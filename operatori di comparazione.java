40f0f695809ca2a1e2c630d0e85a6902d6bdb8ce
simonecordisco <147503322+simonecordisco@users.noreply.github.com>
2023-10-13 10:51:53 +0200

    Add files via upload

---
+++ operatori di comparazione.java
@@ -0,0 +1,14 @@
+
+//Scrivi un programma che contiene un metodo che che confronti due numeri interi e determini se sono diversi.
+//Il programma dovrà stampare a video i due valori e il risultato dell'eguaglianza.
+public class MyClass {
+    public static void main(String args[]) {
+      int x=2;
+      int y=2;
+      System.out.println(x);
+      System.out.println(y);
+      boolean equals = x==y;
+
+      System.out.println(equals);
+    }
+}
