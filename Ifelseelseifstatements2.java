5c6d9d4f8203aeaf9cf2e6db115613d2f31388bd
simonecordisco <147503322+simonecordisco@users.noreply.github.com>
2023-10-13 13:00:51 +0200

    Add files via upload

---
+++ Ifelseelseifstatements2.java
@@ -0,0 +1,13 @@
+public class MyClass {
+    public static void main(String args[]) {
+    String nome ="10";
+     int lunghezza = nome.lenght();
+     System.out.println(lunghezza);
+      if(lunghezza >10 || lunghezza <10){
+    System.out.println("lunghezza maggiore di 10" + lunghezza);
+}     else{
+    System.out.println("lunghezza minore di 10" + lunghezza);  
+}
+
+    }
+}
