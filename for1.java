d8d81177bf9097268245d9ee8a8a2cdc8d85b44c
simonecordisco <147503322+simonecordisco@users.noreply.github.com>
2023-10-13 14:41:56 +0200

    Add files via upload

---
+++ for1.java
@@ -0,0 +1,11 @@
+
+public class MyClass {
+    public static void main(String args[]) {
+      
+     for(int y=0; y<=10; y++){
+          int x=2;
+           int s=x*y;
+        System.out.println (s);
+      }
+      }
+}
