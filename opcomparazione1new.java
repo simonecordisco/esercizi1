0f137c1c401b1a8bf71b4ddb844e067b7aa8f202
simonecordisco <147503322+simonecordisco@users.noreply.github.com>
2023-10-25 10:21:37 +0200

    Add files via upload

---
+++ opcomparazione1new.java
@@ -0,0 +1,14 @@
+public class opcomparazione1new {
+    public static void main(String args[]) {
+        int a = 2;
+        int b = 2;
+        System.out.println(a);
+        System.out.println(b);
+        comparazione(a, b);
+    }
+    public static void comparazione(int x, int y){
+        boolean equals = x==y;
+
+        System.out.println(equals);
+    }
+}
