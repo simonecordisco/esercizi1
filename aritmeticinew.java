3d7dba400ad8cbea3951b0fd3180a3194b0c6a4e
simonecordisco <147503322+simonecordisco@users.noreply.github.com>
2023-10-25 09:59:05 +0200

    Add files via upload

---
+++ aritmeticinew.java
@@ -0,0 +1,18 @@
+public class aritmeticinew {
+    public static void main(String[] args) {
+        int a = 40;
+        int b = 20;
+        int c = 30;
+        System.out.println("a =" + a);
+        System.out.println("b =" + b);
+        System.out.println("c =" + c);
+        media(a, b, c);
+    }
+
+    public static void media(int x, int y, int z) {
+        int resultSum = (x + y + z) / 3;
+        System.out.println("la media è " + resultSum);
+    }
+}
+
+
