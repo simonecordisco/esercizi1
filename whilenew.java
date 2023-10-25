a9385741a715c6b9ddbade629226464a356088ba
simonecordisco <147503322+simonecordisco@users.noreply.github.com>
2023-10-25 14:30:36 +0200

    Add files via upload

---
+++ whilenew.java
@@ -0,0 +1,25 @@
+//Scrivere un programma che contenga un metodo che sfrutti il while loop
+// e prenda in ingresso 1 valore intero come limite superiore e calcoli la
+// somma di tutti i valori prima del limite e la stampi a video.
+//(Esempio passo 5 come limite e otterrò la somma di 1 + 2 + 3 + 4)
+
+public class whilenew {
+    public static void main(String args[]) {
+        int x=5;
+        Risultato(x);
+    }
+
+    private static void Risultato(int x) {
+        int counter = 1;
+        int somma = 0;
+        while(counter < x){
+
+            somma = somma + counter;
+
+            System.out.println ("La somma e' " + somma);
+            counter++;
+        }
+    }
+}
+// non capisco come fermalo a 4 cioè a 10 come risultato
+
